8abbaa7bb97ea84014142521bc3803d423839de6
Ivan R. <41546276+kasper546@users.noreply.github.com>
2024-08-28 08:37:19 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+  <FloatLabel>
+    <InputText id="username" v-model="value" @keydown="onKeydown" :readonly="isReadOnly" @blur="handleBlur"/>
+    <label for="username">https://</label>
+    <div v-if="title">
+      <p>Заголовок страницы: {{ title }}</p>
+    </div>
+</FloatLabel>
+<Button icon="pi pi-pencil" v-if="title" @click="handleButtonClick"></Button>
+</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from 'vue';
+import InputText from 'primevue/inputtext';
+import FloatLabel from 'primevue/floatlabel';
+import 'primeicons/primeicons.css';
+import Button from 'primevue/button';
+const value = ref(''); // Определяем реактивную переменную для ввода
+const title = ref(''); // Создаем реактивную переменную для заголовка
+const isReadOnly = ref(false);
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+async function getPageTitle(url) {
+    try {
+        const response = await fetch(url);
+        
+        // Проверяем, успешно ли выполнен запрос
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const htmlString = await response.text();
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(htmlString, 'text/html');
+
+        // Извлекаем заголовок
+        const titleElement = doc.querySelector('title');
+        const pageTitle = titleElement ? titleElement.innerText : 'Заголовок не найден';
+        
+        return pageTitle;
+    } catch (error) {
+        console.error('Ошибка:', error);
+        return null;
+    }
+}
+
+// Обработчик события keydown
+async function onKeydown(event) {
+    if (event.key === 'Enter') {
+        if (value.value) {
+            console.log('Fetching title for:', value.value);
+            const pageTitle = await getPageTitle(value.value);
+            console.log('Fetched title:', pageTitle);
+            title.value = pageTitle;
+            isReadOnly.value = true;
+        }
+    }
+}
+async function handleBlur() {
+  isReadOnly.value = false;
+    }
+function handleButtonClick() {
+  isReadOnly.value = false;
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+margin-left: 500px;
+margin-top: 500px;
 }
 </style>
